bb9faebb38c3096e6fbf7775029808f5246c462e
Raul Correia <raulvinicius245@gmail.com>
2021-09-19 01:16:07 -0300

    Curso concluído!

---
+++ src/com/raul/javabasico/aula0/CursoConcluido.java
@@ -0,0 +1,14 @@
+package com.raul.javabasico.aula0;
+
+public class CursoConcluido {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("********************************");
+		System.out.println("*                              *");
+		System.out.println("* Curso conluido :) 19/09/2021 *");
+		System.out.println("*                              *");
+		System.out.println("********************************");
+	}
+
+}

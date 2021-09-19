1027b1f083c687a0be84e468c9957882df9a1181
Raul Correia <raulvinicius245@gmail.com>
2021-09-18 22:12:32 -0300

    Utilizando o finally.

---
+++ src/com/raul/javabasico/aula49/TestandoFinally.java
@@ -0,0 +1,24 @@
+package com.raul.javabasico.aula49;
+
+public class TestandoFinally {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numeros = {4, 8, 16, 32, 64, 128};
+		int[] denom = {2, 0, 4, 8, 0};
+		
+		for(int i = 0; i < numeros.length; i++) {
+			try {
+				System.out.println(numeros[i]+"/"+denom[i]+" = "+numeros[i]/denom[i]);
+			}catch(ArithmeticException exception){
+				System.err.println("Erro ao dividir numero por zero!");
+			}catch(ArrayIndexOutOfBoundsException exception){
+				System.err.println("Erro ao acessar posição inexistente no array!");
+			}finally {
+				System.out.println("Ação executada sempre após um try ou catch.");
+			}
+		}
+
+	}
+
+}
